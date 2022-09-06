e09065ba30d4f3bcde16522a7dff17d8b6c85b91
SG1515 <sunggu507@naver.com>
2022-09-06 16:35:15 +0900

    t8 테스트케이스 보강

--- src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryCustom.java
+++ src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryCustom.java
@@ -2,6 +2,7 @@ package com.ll.exam.qsl.user.repository;
 
 import com.ll.exam.qsl.user.entity.SiteUser;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
